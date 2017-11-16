787a16bc4ebf9431d2ee8bf3f0d5e6fcc92bc4ab
Miwan9 <kozhema7@gmail.com>
2017-11-16 21:26:28 +0200

    first

---
+++ js.js
@@ -0,0 +1,39 @@
+//Task 1
+// function numb1 (){
+// 	for (var i=4; i<401; i=i+1){
+// 		document.getElementById('out').innerHTML+=i+' ';
+// 	}
+// }
+// numb1();
+
+//Task2
+// function numb2 (){
+// 	for (var i=4; i<401; i=i+3){
+// 		document.getElementById('out').innerHTML+=i+' ';
+// 	}
+// }
+// numb2();
+
+//Task3
+// function numb3 (){
+// 	for (var i=654; i>0; i=i-1){
+// 		document.getElementById('out').innerHTML+=i+' ';
+// 	}
+// }
+// numb3();
+
+//Task4
+// function numb4 (){
+// 	for (var i=1983; i<=2017; i=i+1){
+// 		document.getElementById('out').innerHTML+=i+' ';
+// 	}
+// }
+// numb4();
+
+//Task5
+// function numb5 (){
+// 	for (var i=-4; i<=100; i=i+2){
+// 		document.getElementById('out').innerHTML+=i+' ';
+// 	}
+// }
+// numb5();
